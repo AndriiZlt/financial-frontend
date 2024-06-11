c1aee3c3a5fdba3d23381ba51347acbcfba9e533
AndriiZlt <117361082+AndriiZlt@users.noreply.github.com>
2024-06-11 18:20:15 +0300

    init2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)},
-  {path: 'members', loadChildren: () => import('./modules/pages/pages.module').then(m => m.PagesModule)}
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./features/auth/auth.module").then((m) => m.AuthModule),
+  },
+  {
+    path: "members",
+    loadChildren: () =>
+      import("./features/pages/pages.module").then((m) => m.PagesModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
